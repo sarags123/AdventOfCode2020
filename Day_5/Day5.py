75a3e597a45335fee5026a356d8b316ae28e13c4
sarags123 <75037830+sarags123@users.noreply.github.com>
2021-12-11 15:09:36 -0500

    Add files via upload

--- Day_5/Day5.py
+++ Day_5/Day5.py
@@ -6,8 +6,8 @@ col = 0
 collowerIndex = 0
 colupperIndex = 7
 idList = []
-ID = 0
-for item in f.readlines():
+ID = 0 #setting up possible variables to use
+for item in f.readlines(): #iterating through each line of file
     rowlowerIndex = 0
     rowupperIndex = 127
     row = 0
@@ -16,7 +16,7 @@ for item in f.readlines():
     colupperIndex = 7
     for i in item:
         if i == 'F': #f takes the lower half
-            rowupperIndex = int((rowlowerIndex+rowupperIndex+1)/2) - 1
+            rowupperIndex = int((rowlowerIndex+rowupperIndex+1)/2) - 1 
         elif i == 'B': #b takes the upper half
             rowlowerIndex = int((rowlowerIndex+rowupperIndex+1)/2)
         elif i == 'L': #l takes the lower half
@@ -25,14 +25,23 @@ for item in f.readlines():
             collowerIndex = int((collowerIndex+colupperIndex+1)/2)
 
     row = rowlowerIndex
-    col = collowerIndex
-    ID = row * 8 + col
+    col = collowerIndex 
+    ID = row * 8 + col #returning the id number
     idList.append(ID)
+        
 bigID = 0
-for passID in idList: 
+for passID in idList: #iterating through list of ids to find the largest id
     if passID > bigID :
         bigID = passID
 print(bigID)
+idList.sort() #putting list of ids in ascending order
 print(idList)
+myID = []
+for passID in idList: #finding missing boarding pass id in the list
+    if (passID + 1 in idList) and (passID - 1 in idList):
+        continue
+    else:
+        myID.append(passID)
+print(myID) #will give the two id numbers of the boarding passes right next to your take the number in between for answer to pt2 
+        
 
-    
