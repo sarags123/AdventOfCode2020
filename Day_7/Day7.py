da5e0679edefb5fd169d7055026bf96bab1d04d5
Saragha <sarags123@gmail.com>
2022-01-20 16:55:42 -0500

    Day 7 comments

--- Day_7/Day7.py
+++ Day_7/Day7.py
@@ -1,15 +1,15 @@
 f = open("Day7PuzzleInput.txt") 
 
-rules = {}
+rules = {} #creating a list of rules with shiny gold bag
 for rule in f:
-    rule = rule.split(" bags contain ")
-    rule[1] = rule[1].split(",")
-    bags = []
-    for bag in rule[1]:
+    rule = rule.split(" bags contain ") #split between bag and what is inside bag
+    rule[1] = rule[1].split(",") #splits between different bags inside of bag
+    bags = [] #creating a list of bags
+    for bag in rule[1]: #looping through different bags inside of bag
         bag = bag.strip(" ")
-        bags.append(bag.split(" "))
+        bags.append(bag.split(" "))#removes the number
     contained_bags = []
-    for bag in bags:
+    for bag in bags: 
         bag = bag[1] + " " + bag[2]
         contained_bags.append(bag)
     if "other" in contained_bags[0]:
@@ -18,7 +18,7 @@ for rule in f:
 
 bags = ["shiny gold"]
 
-for bag in bags:
+for bag in bags: #for loop to append the bag to the bags list if it has shiny gold 
     for rule in rules:
         if bag in rules[rule]:
             if rule not in bags:
